67dcecfe89965a1e7f582d195d71e13a7fa7af4b
Brook Mezgebu <brookmezgebu64@gmail.com>
2020-10-25 21:32:27 +0300

    test

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const Knex = require('knex');
 
-const state = process.env.NODE_ENV || 'development';
+const state = process.env.NODE_ENV || 'normal';
 const dotenv = require('dotenv');
 dotenv.config();
+
+const connectionString = state == "testing" ? process.env.DATABASE_URL : process.env.TEST_DATABASE_URL
 
 // These values might change with some env values
 // This is the instance in which all models get db access from
 const KnexI = Knex({
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: connectionString
 });
 
-console.log(process.env.DATABASE_URL)
+console.log(connectionString)
 
 async function f() {
     console.log(await KnexI.raw("select * from one;"))
